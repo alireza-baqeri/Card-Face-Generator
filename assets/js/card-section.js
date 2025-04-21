70668a3acb35313c3a711b24bfb796a5148c4f5e
Alireza Bagheri <abrahamgoldman13@gmail.com>
2025-04-21 20:44:54 +0330

    selecting and controlling btns

--- assets/js/card-section.js
+++ assets/js/card-section.js
@@ -1,7 +1,101 @@
-'use strict'
+'use strict'// @ts-nocheck
 // selecting the number on the card 
 const numerOnTheCard = document.getElementById("card-num")
 // selecting the shaba on the card
 const shabaOnTheCard = document.getElementById("shaba-num")
 //selecting the name on the card
 const nameOnTheCard = document.getElementById("card-name")
+//selecting the Cards background
+const cardBackground = document.querySelector(".card-container")
+// Selecting the Card Numbers
+const cardNumberColor = document.querySelectorAll(".card-num,.shaba-num,.card-name")
+
+//⬇️selecting the inputs
+//4inputs for 4digits
+const cardNumberInputOne = document.querySelector("#card-digit-1")
+const cardNumberInputTwo = document.querySelector("#card-digit-2")
+const cardNumberInputThree = document.querySelector("#card-digit-3")
+const cardNumberInputFour = document.querySelector("#card-digit-4")
+// shaba input
+const shabaInput = document.querySelector("#shabaInput")
+//name input
+const nameInput = document.querySelector("#nameInput")
+
+//⬇️ Selecting buttons
+//theme change button
+const themeChangerBtn = document.querySelector(".cardNumbersChangerBtn")
+// selecting the reset button
+const resetBtn = document.querySelector(".cardResetBtn")
+//selecting the builder button 
+const builderBtn = document.querySelector(".cardBuilderBtn")
+//selecting card numbers color changer btn
+const cardNumbersColorChangerBtn = document.querySelector(".cardNumbersColorChangerBtn")
+//selecting picture generator btn
+const cardDownloaderBtn = document.querySelector(".cardDownloaderBtn")
+
+cardNumberInputOne.addEventListener("input", function (e) {
+    let eValue = e.target.value;
+    if (!isNaN(eValue) && eValue !== "") { // چک کن عددی باشه
+        numerOnTheCard.textContent = eValue;
+
+    }
+})
+
+// cardNumberInput.forEach(input => {
+//     input.addEventListener("input",function(e)){
+//         const value = e.target.value;
+//         if(!isNaN())
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
