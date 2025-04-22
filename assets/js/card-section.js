42c4efbd8a6c30559d0097a4a69ab5a66ae11bd4
Alireza Bagheri <abrahamgoldman13@gmail.com>
2025-04-22 12:56:41 +0330

    adding the regex fot inputs

--- assets/js/card-section.js
+++ assets/js/card-section.js
@@ -27,19 +27,37 @@ const themeChangerBtn = document.querySelector(".cardNumbersChangerBtn")
 // selecting the reset button
 const resetBtn = document.querySelector(".cardResetBtn")
 //selecting the builder button 
-const builderBtn = document.querySelector(".cardBuilderBtn")
+const builderBtn = document.querySelector(".buildCardBtn")
 //selecting card numbers color changer btn
 const cardNumbersColorChangerBtn = document.querySelector(".cardNumbersColorChangerBtn")
 //selecting picture generator btn
 const cardDownloaderBtn = document.querySelector(".cardDownloaderBtn")
 
-cardNumberInputOne.addEventListener("input", function (e) {
-    let eValue = e.target.value;
-    if (!isNaN(eValue) && eValue !== "") { // چک کن عددی باشه
-        numerOnTheCard.textContent = eValue;
+let enteredCardNum = "";
 
-    }
+builderBtn.addEventListener("click", function () {
+
+    enteredCardNum = cardNumberInputOne.value + " " + cardNumberInputTwo.value + " " + cardNumberInputThree.value + " " + cardNumberInputFour.value
+    console.log(enteredCardNum)
 })
+
+
+// cardNumberInputOne.addEventListener("input", function (e) {
+//     let eValue = e.target.value;
+//     if (!isNaN(eValue) && eValue !== "") { // چک کن عددی باشه
+//         numerOnTheCard.textContent = eValue;
+//
+//     }
+// })
+
+
+// cardNumberInputOne.addEventListener("input", function (e) {
+//     let eValue = e.target.value;
+//     if (!isNaN(eValue) && eValue !== "") { // چک کن عددی باشه
+//         numerOnTheCard.textContent = eValue;
+//
+//     }
+// })
 
 // cardNumberInput.forEach(input => {
 //     input.addEventListener("input",function(e)){
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,23 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
 
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll(".nav-link");
-
-    window.addEventListener("scroll", () => {
+window.addEventListener("scroll", () => {
     let current = "";
     sections.forEach((section) => {
-    const top = section.offsetTop;
-    const height = section.offsetHeight;
-    if (scrollY >= top - height / 3) {
-    current = section.getAttribute("id");
-}
-});
+        const top = section.offsetTop;
+        const height = section.offsetHeight;
+        if (scrollY >= top - height / 3) {
+            current = section.getAttribute("id");
+        }
+    });
 
     navLinks.forEach((link) => {
-    link.classList.remove("active");
-    if (link.getAttribute("href") === "#" + current) {
-    link.classList.add("active");
-}
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
 });
-});
+
+
 
