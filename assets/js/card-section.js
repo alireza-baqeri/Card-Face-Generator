306b11e9a0be2d6e4ca5c10a9d719f22c0aca15d
Alireza Bagheri <abrahamgoldman13@gmail.com>
2025-04-24 22:42:23 +0330

    validation of the entered values

--- assets/js/card-section.js
+++ assets/js/card-section.js
@@ -1,10 +1,10 @@
 'use strict'// @ts-nocheck
 // selecting the number on the card 
 const numberOnTheCard = document.getElementById("card-num")
-const firstNumberOnTheCard = document.querySelector(".cardDigit-1").value
-const secondNumberOnTheCard = document.querySelector(".cardDigit-2").value
-const thirdNumberOnTheCard = document.querySelector(".cardDigit-3").value
-const fourthNumberOnTheCard = document.querySelector(".cardDigit-4").value
+const firstNumberOnTheCard = document.querySelector(".cardDigit-1")
+const secondNumberOnTheCard = document.querySelector(".cardDigit-2")
+const thirdNumberOnTheCard = document.querySelector(".cardDigit-3")
+const fourthNumberOnTheCard = document.querySelector(".cardDigit-4")
 // selecting the shaba on the card
 const shabaOnTheCard = document.getElementById("shaba-num")
 //selecting the name on the card
@@ -21,7 +21,7 @@ const cardNumberInputTwo = document.querySelector("#card-digit-2")
 const cardNumberInputThree = document.querySelector("#card-digit-3")
 const cardNumberInputFour = document.querySelector("#card-digit-4")
 // shaba input
-const shabaInput = document.querySelector("#shabaInput").value
+const shabaInput = document.querySelector("#shabaInput")
 //name input
 const nameInput = document.querySelector("#nameInput")
 
@@ -38,29 +38,77 @@ const cardNumbersColorChangerBtn = document.querySelector(".cardNumbersColorChan
 const cardDownloaderBtn = document.querySelector(".cardDownloaderBtn")
 
 
-builderBtn.addEventListener("click", function () {
+
+
+
+
+builderBtn.addEventListener("click", function (e) {
+     e.preventDefault();
+    // card
     const one = cardNumberInputOne.value;
     const two = cardNumberInputTwo.value;
     const three = cardNumberInputThree.value;
     const four = cardNumberInputFour.value;
-    const shaba = shabaInput.value;
+
+    // if (shabaInput.value.length === 20) { 
+    //     const shaba = shabaInput.value;
+    // } else {
+    //     const shaba = 0;
+    // }
+    
+    // if (typeof(Number(shabaInput.value)) === "number") {
+      
+    // } else {
+    //     const shabaValid = false
+    // }
+    // const shabaNull = typeof(shabaInput.value) ? Number: false ;
+    // let shaba
+    // if (!shabaNull) { 
+    //     shaba = shabaInput.value; 
+    // }
 
     const allDigitsFilled = one.length === 4 && two.length === 4 && three.length === 4 && four.length === 4;
+    // shaba
     const shabaValid = shaba.length === 20 || shaba.length === 0;
+    // name
+    const personName = nameOnTheCard.value;
+    const nameValid = personName.replace(" ", "").length > 3;
 
-    if (allDigitsFilled && shabaValid) {
+    let errors = 0;
+
+
+    if (allDigitsFilled) {
         firstNumberOnTheCard.textContent = one;
         secondNumberOnTheCard.textContent = two;
         thirdNumberOnTheCard.textContent = three;
         fourthNumberOnTheCard.textContent = four;
 
-        if (shaba.length === 20) {
-            shabaOnTheCard.textContent = shaba;
-        }
     } else {
-        const myModal = new bootstrap.Modal(document.getElementById('warningModal'));
-        myModal.show();
         document.querySelector(".wrongCardNumber").classList.remove("d-none");
+        errors++;
+    }
+
+    if (shabaValid) {
+        shabaOnTheCard.textContent = shaba;
+    } else {
+        if (shaba.value.length === 0) {
+            shabaOnTheCard.textContent = ' ';
+        } else {
+            document.querySelector(".wrongShaba").classList.remove("d-none");
+            errors++;
+        }
+    }
+    if (nameValid) {
+        personName.textContent = nameInput;
+    } else {
+        document.querySelector(".wrongName").classList.remove("d-none");
+        errors++;
+    }
+    if (errors > 0) {
+        const myModal = new bootstrap.Modal(
+          document.getElementById("warningModal")
+        );
+        myModal.show();
     }
 });
 
